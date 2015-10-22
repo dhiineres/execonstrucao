d9b0af9a98e5568b1de7949425f42f7aa664f8c1
alunoinf <alunoinf@pc-649301.(none)>
2015-10-22 19:55:37 -0200

    adicionados testes

--- SomaNatural/src/test/java/br/ufg/inf/es/construcao/somanaturais/SomaTest.java
+++ SomaNatural/src/test/java/br/ufg/inf/es/construcao/somanaturais/SomaTest.java
@@ -8,6 +8,11 @@ public class SomaTest {
         Soma.somatorio(0);
     }
 
+    @Test (expected = IllegalArgumentException.class)
+    public void testNInvalido2() throws Exception {
+        Soma.somatorio(-5);
+    }
+
     @Test
     public void testSomatorio() throws Exception {
         Assert.assertEquals(21, Soma.somatorio(6));
@@ -15,6 +20,7 @@ public class SomaTest {
         Assert.assertEquals(3, Soma.somatorio(2));
         Assert.assertEquals(6, Soma.somatorio(3));
         Assert.assertEquals(15, Soma.somatorio(5));
+        Assert.assertEquals(51*25, Soma.somatorio(50));
     }
 }
 
